d5fe7bc7de4d95c800f63fc6962ce9027d7dd3ce
alidarovolj <oljasalidarov@gmail.com>
2023-11-20 13:21:22 +0600

    first commit

---
+++ app.vue
@@ -0,0 +1,11 @@
+<script setup>
+const layout = "default";
+const {locale} = useI18n();
+
+</script>
+
+<template>
+  <div>
+    {{ locale }}
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,67 @@
+export default defineNuxtConfig({
+  ssr: true,
+  components: [{
+    path: '~/components',
+    pathPrefix: false,
+  }],
+  imports: {
+    dirs: ["store", 'composables', 'composables/*/index.{ts,js,mjs,mts}', 'composables/**'],
+  },
+  devtools: { enabled: false },
+  css: ["assets/css/style.css", "assets/css/fonts.css"],
+  modules: ['@pinia/nuxt', '@nuxtjs/i18n', "@pinia/nuxt"],
+  plugins: ['~/plugins/maska.ts'],
+  runtimeConfig: {
+    public: {
+      API_LINK: process.env.API_LINK,
+    },
+  },
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  // i18n: {
+  //   lazy: true,
+  //   langDir: 'locales',
+  //   strategy: "prefix_except_default",
+  //   defaultLocale: 'ru-RU',
+  //   locales: [
+  //     {
+  //       code: "en-US",
+  //       iso: "en-US",
+  //       name: "English",
+  //       sh_name: "Eng",
+  //       file: "~/locales/en-US.json"
+  //     },
+  //     {
+  //       code: "ru-RU",
+  //       iso: "ru-RU",
+  //       name: "Русский",
+  //       sh_name: "Рус",
+  //       file: "~/locales/ru-RU.json"
+  //     }
+  //   ],
+  //   detectBrowserLanguage: false,
+  //   vueI18n: './i18n.config.ts'
+  // },
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: "[name].js",
+        chunkFileNames: "[name].js",
+      },
+    },
+    transpile: ['@vuepic/vue-datepicker']
+  },
+  pinia: {
+    autoImports: ["defineStore", ["defineStore", "definePiniaStore"]],
+  },
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+})
